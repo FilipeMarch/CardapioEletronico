721f1ec125ad5c46a2cc45ba3f4f89d2b533d42f
DaviPacheco <davipachecosouza@gmail.com>
2016-11-02 20:25:25 -0300

    Alterei a sintaxe pra groovy

--- app/src/main/groovy/davifilipe/cardapioeletronico/CardapioEletronicoMainActivity.groovy
+++ app/src/main/groovy/davifilipe/cardapioeletronico/CardapioEletronicoMainActivity.groovy
@@ -3,11 +3,10 @@ package davifilipe.cardapioeletronico;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CardapioEletronicoMainActivity extends AppCompatActivity {
+ class CardapioEletronicoMainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cardapio_eletronico_main);
+        super.onCreate savedInstanceState
+        setContentView R.layout.activity_cardapio_eletronico_main
     }
 }
