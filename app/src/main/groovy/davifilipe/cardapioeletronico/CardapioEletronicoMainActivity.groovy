2e9a27a7b2cc46764d087d21d0b0888e580b58e7
DaviPacheco <davipachecosouza@gmail.com>
2016-11-02 21:19:04 -0300

    Criei um Script pra testes

--- app/src/main/groovy/davifilipe/cardapioeletronico/CardapioEletronicoMainActivity.groovy
+++ app/src/main/groovy/davifilipe/cardapioeletronico/CardapioEletronicoMainActivity.groovy
@@ -1,20 +1,19 @@
-package davifilipe.cardapioeletronico;
+package davifilipe.cardapioeletronico
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle
 
  class CardapioEletronicoMainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate savedInstanceState
-        setContentView R.layout.activity_cardapio_eletronico_main
 
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cardapio_eletronico_main)
 
     }
 
-    void xerecao() {
-        xereca = 'ae xereca'
-    }
+    String nome() { "davi" }
+
 }
 
 
